a9c7198774f14e76448f1c20b97a824631e02098
Michael Norris <github@michaelnorris.co.uk>
2015-09-03 13:58:19 +0100

    add master page, remove welcome page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Flyer</title>
+    <link rel="stylesheet" href="/css/app.css">
+
+    @yield('header')
+
+</head>
+<body>
+
+@yield('content')
+
+
+@yield('footer')
+</body>
+</html>
